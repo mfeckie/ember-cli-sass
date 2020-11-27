feb11227bdaea8cf1f6cfbe85fffcbc90fc856b1
Martin Feckie <mfeckie@gmail.com>
2020-11-27 22:53:57 +0800

    Format file

--- sass-compiler.js
+++ sass-compiler.js
@@ -24,17 +24,17 @@ module.exports = class SassCompiler extends Plugin {
 
     var destFile = path.join(this.outputPath, this.outputFile)
     mkdirp.sync(path.dirname(destFile))
-  
+
     var sassOptions = {
       file: path.join(this.inputPaths[0], this.inputFile),
       includePaths: this.inputPaths,
-  
+
       imagePath: this.options.imagePath,
       outputStyle: this.options.outputStyle,
       precision: this.options.precision,
       sourceComments: this.options.sourceComments,
     }
-  
+
     const result = sass.renderSync(sassOptions)
     fs.writeFileSync(destFile, result.css)
 
